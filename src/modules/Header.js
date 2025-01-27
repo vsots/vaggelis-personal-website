b8ca7e9f59712f514f575cd416274f7eb806bccd
vsots <vsotsdev@gmail.com>
2025-01-27 14:47:08 -0800

    new header

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -12,9 +12,6 @@ class Header extends HTMLElement {
             const content = document.getElementById("content");
 
             switch(el.attributes.id.value) {
-                case "about":
-                    content.innerHTML = `<about-me-page></about-me-page>`;
-                    break;
                 case "skills":
                     content.innerHTML = `<skills-and-projects-page></skills-and-projects-page>`;
                     break;
@@ -31,62 +28,41 @@ class Header extends HTMLElement {
 const headerCss = `
     #header {
         box-sizing: border-box;
-        height: 17vh;
-        padding: 2em 2em 0 2em;
-        border-bottom: 1px black solid;
-        position: sticky;
+        position: fixed;
         top: 0;
+        left: 0;
+        width: 100%;
         background-color: white;
-    }
-
-    .title {
+        padding: 1rem 1.5rem;
+        border-bottom: 1px black solid;
         display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
+        justify-content: space-between;
         align-items: center;
-        text-align: center;
     }
 
-    img {
-        border: 1px black solid;
-        border-radius: 100%;
-        height: 10vh;
-        width: 10vh;
-    }
-
-    .name > h1 {
-        font-size: 3em;
-        line-height: 0.5;
-    }
-
-    .name > h2 {
-        font-size: 2em;
-        line-height: 0.5;
+    #name {
+        font-size: 2.15rem;
+        font-weight: 500;
+        margin: 0;
     }
 
     #menu {
         display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        align-items: flex-end;
-        height: 5vh;
-        font-size: 1.5em;
+    }
+    
+    #menu > h3 {
+        padding: 0 0.7rem;
+        font-size: 1.1rem;
+        margin: 0;
     }
 `;
 
 const headerHtml = `
     <style>${headerCss}</style>
     <div id="header">
-        <div class="title">
-            <img src="./media/Vaggelis_Sotiropoulos_Profile_Photo.jpeg" alt="Welcome"/>
-            <div class="name">
-                <h1>Vaggelis Sotiropoulos</h1>
-                <h2>Software Engineer</h2>
-            </div>
-        </div>
+        <h3 id="name">Vaggelis Sotiropoulos</h3>
         <div id="menu">
             <h3 id="home">Home</h3>
-            <h3 id="about">About Me</h3>
             <h3 id="skills">Skills & Projects</h3>
             <h3 id="contact">Contact</h3>
         </div>
