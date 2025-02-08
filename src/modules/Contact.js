0526767900317c2aa13cf42360ba5bbfe4092c4d
vsots <vsotsdev@gmail.com>
2025-02-08 15:23:57 -0800

    edits fetch url

--- src/modules/Contact.js
+++ src/modules/Contact.js
@@ -28,7 +28,7 @@ class Contact extends HTMLElement {
             const json = JSON.stringify({name, email, "subject": (subject.length ? subject : 'No Subject'), message});
 
             try {
-                const res = await fetch('http://127.0.0.1:8080/contactsubmission', {
+                const res = await fetch('https://api.vaggelissengineer.com/contactsubmission', {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
